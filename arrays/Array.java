6dd36598835dce341efcf444dbeb96928117bb05
JoãoPaulo <joaopaulosg17@gmail.com>
2023-03-20 11:12:22 -0300

    Estudo sobre estrutura de dados: Arrays

---
+++ arrays/Array.java
@@ -0,0 +1,18 @@
+package arrays;
+public class Array {
+    public static void main(String[] args) {
+        /* Arrays unidimensionais */
+
+        //Inicialização do array
+        int[] array = {1,2,3};
+        
+        //Acesso a item no array
+        System.out.println(array[1]);
+
+        /* Arrays bidimensionais */
+        int[][] array2 = {{1,2,3},{1,2,3},{1,2,3}};
+
+        //Acesso a item no array
+        System.out.println(array2[0][1]);
+    }
+}
